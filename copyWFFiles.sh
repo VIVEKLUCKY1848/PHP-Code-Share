6c71f416e995ffaa1fa64215dec9c1670b7de8eb
VS <VIVEKLUCKY1848@users.noreply.github.com>
2016-06-27 11:56:47 +0530

    Add files via upload

--- copyWFFiles.sh
+++ copyWFFiles.sh
@@ -2,5 +2,7 @@
 CURR_DIR=$PWD;
 TARGET_DIR="$PWD/$2";
 while IFS='' read -r line || [[ -n "$line" ]]; do
-	test -d "$TARGET_DIR" || mkdir -p "$TARGET_DIR" && cp --parents $line "$TARGET_DIR"
+	if [[ ! "$line" =~ "~" && ! "$line" =~ "$1" ]]; then
+		test -d "$TARGET_DIR" || mkdir -p "$TARGET_DIR" && cp --parents $line "$TARGET_DIR"
+	fi
 done < "$1"
